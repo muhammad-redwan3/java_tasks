74f69efc16d9bf2690aadb68feb40ac9ec69512a
Muhammad Redwan <68433972+muhammad-redwan3@users.noreply.github.com>
2022-05-27 22:26:38 +0300

    ClassStatus

---
+++ src/Kodluyoruz/ChineseZodiac.java
@@ -0,0 +1,31 @@
+package Kodluyoruz;
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int birthYear;
+        String burc = "";
+
+        System.out.print("Doğum yılınızı giriniz : ");
+        birthYear = input.nextInt();
+
+        switch (birthYear % 12) {
+            case 0 -> burc = "Maymun";
+            case 1 -> burc = "Horoz";
+            case 2 -> burc = "Köpek";
+            case 3 -> burc = "Domuz";
+            case 4 -> burc = "Fare";
+            case 5 -> burc = "Öküz";
+            case 6 -> burc = "Kaplan";
+            case 7 -> burc = "Tavşan";
+            case 8 -> burc = "Ejderha";
+            case 9 -> burc = "Yılan";
+            case 10 -> burc = "At";
+            case 11 -> burc = "Koyun";
+            default -> System.out.print("Hata!");
+        }
+        System.out.print("Çin Zodyağı burcunuz : " + burc);
+    }
+}
